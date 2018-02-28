ee4fbc5637fa4b2786a7ce9bac9d30991b77503c
ivy <zhengyumeng1125@gmail.com>
2018-02-27 18:27:52 -0800

    added template engine

--- server/server.js
+++ server/server.js
@@ -1,20 +1,25 @@
 const express = require('express');
 const morgan = require('morgan');
+const pug = require('pug');
 const path = require('path');
-const request = require('request');
 const app = express();
 const port = process.env.PORT || 8000;
-const bodyParser = require('body-parser');
-
 
 app.use(morgan('dev'));
+app.set('view engine', 'pug');
+app.use(express.static(path.join(__dirname, '../views')));
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use('/:id/', (req, res) => {
+//   // console.log(req.params.id)
+//   // var id = req.params.id;
+// });
+app.get('/:id/', (req, res) => {
+  console.log('00330')
+  let ida = req.params.id;
+  res.render('index', { id: 10 });
+});
 
-app.use(express.static(path.join(__dirname, '../public')));
-app.use('/:id/', express.static(path.join(__dirname, '../public')));
-
+// app.use('/:id/', express.static(path.join(__dirname, '../public')));
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });

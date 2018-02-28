f30141d15aea8212193b529b3901f61eb2d33f1e
ivy <zhengyumeng1125@gmail.com>
2018-02-27 22:12:32 -0800

    git added real link to image

--- server/server.js
+++ server/server.js
@@ -9,17 +9,11 @@ app.use(morgan('dev'));
 app.set('view engine', 'pug');
 app.use(express.static(path.join(__dirname, '../views')));
 
-// app.use('/:id/', (req, res) => {
-//   // console.log(req.params.id)
-//   // var id = req.params.id;
-// });
 app.get('/:id/', (req, res) => {
-  console.log('00330')
   const idNum = req.params.id;
   res.render('index', { id: idNum });
 });
 
-// app.use('/:id/', express.static(path.join(__dirname, '../public')));
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
 });

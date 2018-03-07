fabbbb58476e0ef4878aa0c5293ff4cb71fccd58
ivy <zhengyumeng1125@gmail.com>
2018-03-06 17:28:24 -0800

    changed to serve

--- server/server.js
+++ server/server.js
@@ -6,13 +6,13 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.use(morgan('dev'));
-app.set('view engine', 'pug');
-app.use(express.static(path.join(__dirname, '../views')));
+// app.set('view engine', 'pug');
+app.use('/product/:id/', express.static(path.join(__dirname, '../public')));
 
-app.get('/product/:id/', (req, res) => {
-  const idNum = req.params.id;
-  res.render('index', { id: idNum });
-});
+// app.get('/product/:id/', (req, res) => {
+//   const idNum = req.params.id;
+//   res.render('index', { id: idNum });
+// });
 
 app.listen(port, () => {
   console.log(`server running at: http://localhost:${port}`);
